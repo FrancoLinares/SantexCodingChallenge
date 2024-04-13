24888a0600b4e3e8343be115d67c6b6816a35a16
Franco Linares <franco.linares@gmail.com>
2024-04-13 16:36:01 -0300

    feat: add code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
+import { Suspense, lazy } from 'react';
 import { ApolloProvider } from '@apollo/client';
-import { Header } from './components/Header/Header';
-import { ProductList } from './components/Products/ProductList/ProductList';
+import { OrderProvider } from './providers/OrderProvider';
 import apolloClient from './apollo/client';
 import ErrorBoundary from './components/ErrorBoundary';
-import { OrderProvider } from './providers/OrderProvider';
+import Loading from './components/UI/Loading';
+// Code splitting
+const Header = lazy(() =>
+  import('./components/Header/Header').then(({ Header }) => ({
+    default: Header,
+  }))
+);
+const ProductList = lazy(() =>
+  import('./components/Products/ProductList/ProductList').then(
+    ({ ProductList }) => ({ default: ProductList })
+  )
+);
 
 function App() {
   return (
     <>
-      <ApolloProvider client={apolloClient}>
-        <OrderProvider>
-          <ErrorBoundary>
-            <Header />
-            <div>
-              <ProductList />
-            </div>
-          </ErrorBoundary>
-        </OrderProvider>
-      </ApolloProvider>
+      <Suspense fallback={<Loading />}>
+        <ApolloProvider client={apolloClient}>
+          <OrderProvider>
+            <ErrorBoundary>
+              <Header />
+              <div>
+                <ProductList />
+              </div>
+            </ErrorBoundary>
+          </OrderProvider>
+        </ApolloProvider>
+      </Suspense>
     </>
   );
 }
