f978b5939aba509363dac80b26883783d99bbd35
Franco Linares <franco.linares@gmail.com>
2024-04-13 14:24:53 -0300

    fix: increase the hierarchy of the styled grid item

--- src/components/Products/ProductList/styled.tsx
+++ src/components/Products/ProductList/styled.tsx
@@ -2,5 +2,7 @@ import Grid from '@mui/material/Grid';
 import styled from 'styled-components';
 
 export const StyledGridItem = styled(Grid)`
-  max-width: 400px;
+  &&& {
+    max-width: 400px;
+  }
 `;
