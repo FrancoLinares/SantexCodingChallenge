f104f02471c0ded1e2feccb5e8b3010d134b3b43
Franco Linares <franco.linares@gmail.com>
2024-04-13 14:16:13 -0300

    refactor: optimize order context

--- src/hooks/useOrder.tsx
+++ src/hooks/useOrder.tsx
@@ -1,8 +1,8 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useMemo } from 'react';
 import { OrderContext } from '../providers/OrderProvider';
 import useStateWithStorage from './useStateWithStorage';
 
-export const useOrder = () => {
+const useOrder = () => {
   const context = useContext(OrderContext);
 
   if (context === undefined) {
@@ -10,21 +10,24 @@ export const useOrder = () => {
   }
 
   const { setSubTotal: setSubTotalContext } = context;
+  const subTotalContext = useMemo(() => context?.subTotal, [context?.subTotal]);
 
   // Keep the local storage in sync
   const [subTotal, setSubTotal] = useStateWithStorage('subTotal', 0);
   useEffect(() => {
     // If there is a subTotal in the local storage and subTotal of the context has an initial value ( '0' ),
     // update the subTotal of the context to keep them synchronized.
-    if (!!subTotal && context?.subTotal === 0) {
+    if (!!subTotal && subTotalContext === 0) {
       setSubTotalContext(subTotal);
     }
     // If the context subTotal does not have an initial value (items are added to the order),
     // update the subTotal of the local storage to keep them synchronized.
-    else if (!!context?.subTotal) {
-      setSubTotal(context?.subTotal);
+    else if (!!subTotalContext) {
+      setSubTotal(subTotalContext);
     }
-  }, [context?.subTotal, subTotal]);
+  }, [subTotalContext, subTotal]);
 
   return context;
 };
+
+export default useOrder;
